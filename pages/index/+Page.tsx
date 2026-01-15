d9bff896b139731d4c0b92649838f9fb5a265921
Mohan Vaddi <myselfgeek01@gmail.com>
2026-01-16 01:00:42 +0530

    feat: configured shadcn and added app-sidebar

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -1,7 +1,13 @@
+import React from "react";
 export default function Page() {
   return (
-    <>
-      <h1>Fitness Companion</h1>
-    </>
+    <React.Fragment>
+      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        <div className="bg-muted/50 aspect-video rounded-xl" />
+        <div className="bg-muted/50 aspect-video rounded-xl" />
+        <div className="bg-muted/50 aspect-video rounded-xl" />
+      </div>
+      <div className="bg-muted/50 min-h-screen flex-1 rounded-xl md:min-h-min" />
+    </React.Fragment>
   );
 }
